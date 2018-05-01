1224bc316d9e8ed3a6e708bae816d92d185e94bf
Hoanh An <hoanhan@bennington.edu>
2018-05-01 23:37:06 +0000

    finish problem 1

--- python/python_lab/problem1.py
+++ python/python_lab/problem1.py
@@ -15,7 +15,10 @@ def add_two_numbers(num1, num2):
         num2 <int>/<float>: Number 2
 
     Return:
-        Sum of two numbers in int or float. 0 if not valid.
+        Sum of two numbers:
+            - in int if two numbers are int
+            - in float if two numbers are float
+            - 0 if not valid
     """
     if isinstance(num1, int) or isinstance(num1, float):
         if isinstance(num2, int) or isinstance(num2, float):
@@ -32,7 +35,10 @@ def substract_two_numbers(num1, num2):
         num2 <int>/<float>: Number 2
 
     Return:
-        Difference of two numbers in int or float. 0 if not valid.
+        Difference of two numbers: 
+            - in int if two numbers are int
+            - in float if two numbers are float
+            - 0 if not valid
     """
     if isinstance(num1, int) or isinstance(num1, float):
         if isinstance(num2, int) or isinstance(num2, float):
@@ -50,7 +56,10 @@ def multiply(num, times):
         times <int>: Number of times to multiply
 
     Return:
-        Product in int or float. 0 if not valid.
+        Product of a number:
+            - in int if the number is int
+            - in float if the number is float
+            - 0 if not valid
     """
     if not isinstance(times, int):
         return 0
@@ -62,51 +71,89 @@ def multiply(num, times):
         return total
     return 0
 
-def divide(num1, times):
+def divide(num, times):
     """
+    Divide a number by a number of times.
+
+    Params:
+        num <int>/<float>: Number to divide
+        times <int>: Number of times to divide
+
+    Return:
+        Quotient of a number: 
+            - in int if the number is int or float
+            - 0 if not valid
     """
-    if num1 == 0:
+    if num == 0:
         return 0
 
     if times == 1:
-        if isinstance(num1, int) or isinstance(num1, float):
-            return num1
+        if isinstance(num, int) or isinstance(num, float):
+            return num
         return 0
+    
+    if not isinstance(times, int):
+        return 0
+
+    if num < times:
+        return 0
+    
+    counter = 0
+    while num >= 0:
+        num -= times
+        counter += 1
+
+    return counter - 1
+    
+    return 0
 
 if __name__ == '__main__':
     # Test addition
-    assert 10   == add_two_numbers(5, 5)
-    assert 10.0 == add_two_numbers(5.0, 5.0)
-    assert 10.0 == add_two_numbers(5, 5.0)
-    assert 10.0 == add_two_numbers(5.0, 5)
-    assert 0    == add_two_numbers('', 5)
-    assert 0    == add_two_numbers(5, '')
-    assert 0    == add_two_numbers('', '')
+    assert 10    == add_two_numbers(5, 5)
+    assert -10   == add_two_numbers(-5,-5)
+    assert 10.0  == add_two_numbers(5.0, 5.0)
+    assert -10.0 == add_two_numbers(-5.0, -5.0)
+    assert 10.0  == add_two_numbers(5, 5.0)
+    assert 10.0  == add_two_numbers(5.0, 5)
+    assert 0     == add_two_numbers('', 5)
+    assert 0     == add_two_numbers(5, '')
+    assert 0     == add_two_numbers('', '')
 
     # Test subtraction
-    assert 0    == substract_two_numbers(5, 5)
-    assert 0.0  == substract_two_numbers(5.0, 5.0)
-    assert 0.0  == substract_two_numbers(5, 5.0)
-    assert 0.0  == substract_two_numbers(5.0, 5)
-    assert 0    == substract_two_numbers('', 5)
-    assert 0    == substract_two_numbers(5, '')
-    assert 0    == substract_two_numbers('', '')
+    assert 0     == substract_two_numbers(5, 5)
+    assert 0     == substract_two_numbers(-5,-5)
+    assert -10   == substract_two_numbers(-5, 5)
+    assert 10    == substract_two_numbers(5, -5)
+    assert 0.0   == substract_two_numbers(5.0, 5.0)
+    assert 0.0   == substract_two_numbers(-5.0, -5.0)
+    assert -10.0 == substract_two_numbers(-5.0, 5.0)
+    assert 10.0  == substract_two_numbers(5.0, -5.0)
+    assert 0.0   == substract_two_numbers(5, 5.0)
+    assert 0.0   == substract_two_numbers(5.0, 5)
+    assert 0     == substract_two_numbers('', 5)
+    assert 0     == substract_two_numbers(5, '')
+    assert 0     == substract_two_numbers('', '')
 
     # Test multiplication
-    assert 25   == multiply(5, 5)
-    assert 27.5 == multiply(5.5, 5)
-    assert 0    == multiply(5, '')
-    assert 0    == multiply('', 5)
-    assert 0    == multiply('', '')
-    assert 0    == multiply(5, '5')
-    assert 0    == multiply('', 5)
-    assert 0    == multiply(5.5, 5.5)
+    assert 25    == multiply(5, 5)
+    assert -25   == multiply(-5, 5)
+    assert 27.5  == multiply(5.5, 5)
+    assert -27.5 == multiply(-5.5, 5)
+    assert 0     == multiply(5, '')
+    assert 0     == multiply('', 5)
+    assert 0     == multiply('', '')
+    assert 0     == multiply(5, '5')
+    assert 0     == multiply('', 5)
+    assert 0     == multiply(5.5, 5.5)
 
     # Test division
-    assert 0    == divide(0, 5)
-    assert 0    == divide(0, 1)
-    assert 5    == divide(5, 1)
-    assert 5.0  == divide(5.0, 1)
-    # assert 2    == divide(10, 5)
-    # assert 0    == divide(5, 10)
-    # assert 0.5  == divide(5.0, 10.0)
+    assert 0     == divide(0, 5)
+    assert 0     == divide(0, 1)
+    assert 5     == divide(5, 1)
+    assert 5.0   == divide(5.0, 1)
+    assert 2     == divide(10, 5)
+    assert 2     == divide(11, 5)
+    assert 0     == divide(5, 10)
+    assert 2     == divide(10.0, 5)
+    assert 2     == divide(10.5, 5)
+    assert 2     == divide(11.5, 5)
